e22f7a8ee76f6fe09033860b058c2ace181e8e17
YoonSeo4618 <w2035@e-mirim.hs.kr>
2021-08-27 15:52:08 +0900

    module

---
+++ module, package/do.py
@@ -0,0 +1,3 @@
+import my_module # my_module에있는 모든 내용을 출력
+
+my_module.nara()
---
+++ module, package/my_module.py
@@ -0,0 +1,12 @@
+def nara():
+    print('게임 FPS')
+
+def hyojin():
+    print('마라탕 먹기')
+
+date = '2021.8.27'
+
+if __name__ == '__main__':
+    nara()
+    hyojin()
+    print(date)
